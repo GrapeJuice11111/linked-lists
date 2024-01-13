0f569f19b5f993edb7221a712889564342ec3a1a
Yelish <118233081+GrapeJuice11111@users.noreply.github.com>
2024-01-12 19:46:40 -0800

    Create Node Constructor With Different Nodes

---
+++ linked-lists.js
@@ -0,0 +1,40 @@
+class LinkedLists {
+  constructor(value) {
+    this.Node = Node;
+    this.index = index;
+  }
+
+  append(key, value) {
+    return new Node(key, value);
+  }
+
+  prepend(value) {
+    this.value = value;
+  }
+
+  size() {}
+
+  head() {}
+
+  tail() {}
+
+  atIndex(index) {}
+
+  pop() {}
+
+  contains() {}
+
+  findValue() {}
+
+  toString() {}
+}
+
+class Node {
+  constructor(key, value) {
+    this.key = key;
+    this.next = next;
+  }
+}
+
+const list = new LinkedLists(Nodes);
+console.log(Nodes);
