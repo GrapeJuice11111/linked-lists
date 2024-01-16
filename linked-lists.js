65693b0e339a39fc7fd6b285a0033ce3084c1bbf
Yelish <118233081+GrapeJuice11111@users.noreply.github.com>
2024-01-16 12:55:14 -0600

    Commit A Final Solution For Linked Lists With Methods

--- linked-lists.js
+++ linked-lists.js
@@ -1,40 +1,108 @@
 class LinkedLists {
-  constructor(value) {
-    this.Node = Node;
-    this.index = index;
-  }
+  head = new Nodes("H");
 
-  append(key, value) {
-    return new Node(key, value);
+  append(value, index) {
+    let tmp = this.head;
+    while (tmp.next != null) {
+      tmp = tmp.next;
+    }
+    tmp.next = new Nodes(value, index);
   }
 
   prepend(value) {
-    this.value = value;
+    let tmp = this.head;
+    this.head = new Nodes(value);
+    this.head.next = tmp;
+  }
+  head() {
+    return this.head;
+  }
+  tail() {
+    let tmp = this.head;
+    while (tmp.next != null) {
+      tmp = tmp.next;
+    }
+    return tmp;
   }
 
-  size() {}
+  pop() {
+    if (this.head != null) {
+      let tmp = this.head;
+      while (tmp.next.next != null) {
+        tmp = tmp.next;
+      }
+      console.log(tmp);
+      tmp.next = null;
+    }
+  }
 
-  head() {}
+  contains(value) {
+    let tmp = this.head;
+    while (tmp.next != null) {
+      if (tmp.value === value) {
+        return "Present";
+      }
+      tmp = tmp.next;
+    }
+    return "Not Present";
+  }
 
-  tail() {}
+  toString() {
+    let tmp = this.head;
+    let string = "";
+    while (tmp != null) {
+      string += ` -> (${tmp.value})`;
+      tmp = tmp.next;
+    }
+    return string;
+  }
 
-  atIndex(index) {}
+  find(value) {
+    let tmp = this.head;
+    while (tmp != null) {
+      if (tmp.value === value) {
+        return tmp.index;
+      }
+      tmp = tmp.next;
+    }
+  }
 
-  pop() {}
+  at(index) {
+    let tmp = this.head;
+    while (tmp != null) {
+      if (tmp.index === index) {
+        return tmp.value;
+      }
+      tmp = tmp.next;
+    }
+  }
+  size() {
+    let tmp = this.head;
+    let total = 0;
+    while (tmp != null) {
+      total += 1;
+      tmp = tmp.next;
+    }
 
-  contains() {}
-
-  findValue() {}
-
-  toString() {}
+    return total;
+  }
 }
 
-class Node {
-  constructor(key, value) {
-    this.key = key;
+class Nodes {
+  constructor(value = null, index = 0, next = null) {
+    this.value = value;
+    this.index = index;
     this.next = next;
   }
 }
 
-const list = new LinkedLists(Nodes);
-console.log(Nodes);
+const list = new LinkedLists();
+list.append("E", 1);
+list.append("A", 2);
+list.append("P", 3);
+console.log(list);
+console.log(list.contains("E"));
+console.log(list.toString());
+console.log(list.find("P"));
+console.log(list.at(2));
+console.log(list.size());
